2701a77d7cd654581b1df18b15eda3318f2c8abe
Olga Ivashkiv <olgasworking@gmail.com>
2020-07-14 21:12:01 +0300

    hw4 - half work done

--- JS-module/hw4/hw4.js
+++ JS-module/hw4/hw4.js
@@ -256,3 +256,74 @@ addArrayOfCars(cars, 'wrap');
 // Приклад масивів:
 let usersWithId = [{id: 1, name: 'vasya', age: 31, status: false}, {id: 2, name: 'petya', age: 30, status: true}, {id: 3, name: 'kolya', age: 29, status: true}, {id: 4, name: 'olya', age: 28, status: false},];
 let citiesWithId = [{user_id: 3, country: 'USA', city: 'Portland'}, {user_id: 1, country: 'Ukraine', city: 'Ternopil'}, {user_id: 2, country: 'Poland', city: 'Krakow'}, {user_id: 4, country: 'USA', city: 'Miami'},];
+
+function connectCitiesWithUser(arrayUser, arrayCity) {
+    let newArray = [];
+    for (const user of arrayUser) {
+        user.address = [];
+        for (const city of arrayCity) {
+            if (user.id === city.user_id) {
+                user.address.push(city.user_id)
+            }
+        }newArray.push(user);
+    }return newArray;
+}
+
+console.log(connectCitiesWithUser(usersWithId, citiesWithId));
+
+//Та робимо це функцією.При цьому правила отримувати через аргумент.
+// Є масив котрий характеризує правила. Створити скрипт який ітерує цей масив, та робить з кожне правило в окремому блоці.
+// При цому в блоці, номер правила записати в свій блок, текст правила записати в свій окремий блок.
+// Результатом відпрацювання скріпта повинна бути структура яка міститься в блоці wrap файла rule.html
+//
+let rules = [
+				{
+					title: 'Первое правило Бойцовского клуба.',
+					body: 'Никому не рассказывать о Бойцовском клубе.'
+				},
+				{
+					title: 'Второе правило Бойцовского клуба.',
+					body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+				},
+				{
+					title: 'Третье правило Бойцовского клуба.',
+					body: 'В схватке участвуют только двое.'
+				},
+				{
+					title: 'Четвертое правило Бойцовского клуба.',
+					body: 'Не более одного поединка за один раз.'
+				},
+				{
+					title: 'Пятое правило Бойцовского клуба.',
+					body: 'Бойцы сражаются без обуви и голые по пояс.'
+				},
+				{
+					title: 'Шестое правило Бойцовского клуба.',
+					body: 'Поединок продолжается столько, сколько потребуется.'
+				},
+				{
+					title: 'Седьмое правило Бойцовского клуба.',
+					body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+				},
+				{
+					title: 'Восьмое и последнее правило Бойцовского клуба.',
+					body: 'Новичок обязан принять бой.'
+				},
+
+];
+
+function rulesF(rulesArray) {
+    let container = document.createElement('div');
+    container.id = 'container'
+    for (const rule of rulesArray) {
+        let ruleDiv = document.createElement('div');
+        for (const key in rule) {
+            let keyDiv = document.createElement('div');
+            keyDiv.innerText = `${rule[key]}`
+            ruleDiv.appendChild(keyDiv);
+        }container.appendChild(ruleDiv)
+    }document.body.appendChild(container);
+}
+
+rulesF(rules);
+
