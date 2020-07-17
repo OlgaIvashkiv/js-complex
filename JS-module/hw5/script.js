149a936fb061800fa8e2c5315139909e5d7ba8a5
Olga Ivashkiv <olgasworking@gmail.com>
2020-07-17 18:22:57 +0300

    hw5 - 5 tasks done

--- JS-module/hw5/script.js
+++ JS-module/hw5/script.js
@@ -92,3 +92,161 @@ let select1 = new Tags('select','Тег <select> позволяет создат
     'form','Связывает список с формой','required','Список обязателен для выбора перед отправкой формы');
 console.log(select1);
 
+//Line 75
+//Створити об'єкт car, з властивостями модель, виробник, рік випуску, максимальна швидкість,
+// об'єм двигуна. додати в об'єкт функції:
+// -- drive () - яка виводить в консоль "їдемо зі швидкістю {максимальна швидкість} на годину"
+// -- info () - яка виводить всю інформацію про автомобіль
+// -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
+// -- changeYear (newValue) - змінює рік випуску на значення newValue
+// -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і доавляет його в поточний об'єкт car
+let car = {
+    model: 'mercedes b180',
+    producer: 'mercedes-benz',
+    year: 2011,
+    maxSpeed: 220,
+    volume: 2.0,
+    drive: function(){
+    console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`)
+    },
+    info: function () {
+        console.log(`model-${this.model}\n producer- ${this.producer}\n year- ${this.year}\n maxSpeed- ${this.maxSpeed}\n volume-${this.volume}`)
+    },
+    increaseMaxSpeed: function(newSpeed){
+        this.maxSpeed += newSpeed;
+        return this.maxSpeed
+    },
+    changeYear: function (newValue) {
+        this.year = newValue;
+        return this.year;
+    },
+    addDriver: function (driver) {
+    this.driver = driver;
+    return this.driver;
+    }
+};
+car.drive();
+car.info();
+car.increaseMaxSpeed(20);
+console.log(car.maxSpeed);
+car.changeYear(2015);
+console.log(car.year);
+console.log(car.addDriver({name: 'Olga', age: 26, experience: 4}));
+console.log(car.driver);
+
+//Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
+// -- drive () - яка виводить в консоль "їдемо зі швидкістю {максимальна швидкість} на годину"
+// -- info () - яка виводить всю інформацію про автомобіль
+// -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
+// -- changeYear (newValue) - змінює рік випуску на значення newValue
+// -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і доавляет його в поточний об'єкт car
+
+function Car(model,producer, year, maxSpeed, volume) {
+    this.model = model;
+    this.producer = producer;
+    this.year = year;
+    this.maxSpeed = maxSpeed;
+    this.volume = volume;
+    this.drive = function () {
+        console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`)
+    };
+    this.info = function () {
+        console.log(`model-${this.model}\n producer- ${this.producer}\n year- ${this.year}\n maxSpeed- ${this.maxSpeed}\n volume-${this.volume}`)
+    };
+    this.increaseMaxSpeed = function (newSpeed) {
+        this.maxSpeed += newSpeed;
+        return this.maxSpeed
+    };
+    this.changeYear = function (newValue) {
+        this.year = newValue;
+        return this.year;
+    };
+    this.addDriver = function (driver) {
+        this.driver = driver;
+        return this.driver
+    }
+}
+let renault = new Car('logan', 'renault', 2014, 200, 1.4);
+renault.drive();
+renault.info();
+console.log(renault.increaseMaxSpeed(15));
+console.log(renault.changeYear(2017));
+console.log(renault.addDriver({name: 'Sasha', age: 39}));
+
+//Створити клас який дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
+// -- drive () - яка виводить в консоль "їдемо зі швидкістю {максимальна швидкість} на годину"
+// -- info () - яка виводить всю інформацію про автомобіль
+// -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
+// -- changeYear (newValue) - змінює рік випуску на значення newValue
+// -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і доавляет його в поточний об'єкт car
+class Auto{
+    constructor(model, producer, year, maxSpeed, volume) {
+        this.model = model;
+        this.producer = producer;
+        this.year = year;
+        this.maxSpeed = maxSpeed;
+        this.volume = volume;
+    }
+    drive (){
+        console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`)
+    }
+    info(){
+        console.log(`model-${this.model}\n producer- ${this.producer}\n year- ${this.year}\n maxSpeed- ${this.maxSpeed}\n volume-${this.volume}`)
+    }
+    increaseMaxSpeed(newSpeed){
+        this.maxSpeed += newSpeed;
+        return this.maxSpeed
+    }
+    changeYear(newValue){
+        this.year = newValue;
+        return this.year;
+    }
+    addDriver(driver){
+        this.driver = driver;
+        return this.driver
+    }
+}
+
+let newRenault = new Auto('sandero', 'renault', 2014, 200, 1.4);
+newRenault.drive();
+newRenault.info();
+newRenault.increaseMaxSpeed(20);
+newRenault.changeYear(2013);
+console.log(newRenault.addDriver({name: 'Alex', age: 35}));
+
+//-створити класс попелюшка з полями ім'я, вік, розмір ноги
+// --Створити 10 попелюшок , покласти їх в масив
+// --Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
+// -- за допоиоги циклу знайти яка попелюшка повинна бути з принцом
+class Human {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Cinderella extends Human{
+    constructor(name, age, footSize) {
+        super(name, age);
+        this.footSize = footSize
+    }
+}
+
+new Cinderella()
+
+class Prince extends Human{
+    constructor(name, age, shoeSize) {
+        super(name, age);
+        this.shoeSize = shoeSize;
+    }
+    findCinderella(array){
+        for (const element of array) {
+            if (footSize.element === this.shoeSize){
+                console.log(`My name is ${this.name} and my Cinderella is ${size.name}`)
+            }
+        }
+    }
+}
+
+
+
