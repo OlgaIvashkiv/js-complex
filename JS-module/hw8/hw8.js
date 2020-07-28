69f56abebd45de0549276cf6af60f824039d2eaa
Olga Ivashkiv <olgasworking@gmail.com>
2020-07-28 23:32:37 +0300

    hw8 - complete

--- JS-module/hw8/hw8.js
+++ JS-module/hw8/hw8.js
@@ -83,3 +83,112 @@ btnRight.onclick=()=>{
     }
     textarea2.value = localStorage.getItem(+index+1)
 }
+//Реализуйте записную книгу, хранящую данные в локальном хранилище.
+// Данные которые надо сохранять : ФИО, номер, почта, фирма, отдел, день рождения
+// Данные вводить через соответсвующую форму.
+// --Каждому контакту добавить кнопку для удаления контакта.
+// --Каждому контакту добавить кнопку редактироваиня. При нажати на нее появляется форма,
+// в которой есть все необходимые инпуты для редактирования, которые уже заполнены данными объекта
+let contactForm = document.forms.contactForm;
+let btnSave = document.getElementById('btnSave');
+
+const usersArray = 'USERS_ARRAY';
+let tempUser = {};
+let contactDiv = document.getElementById('contactDiv');
+
+btnSave.onclick = (ev)=>{
+    let user = {...tempUser};
+    tempUser = {};
+    for (const formElement of contactForm) {
+        if (formElement.name){
+            user[formElement.name] = formElement.value;
+        }
+    }if (!user.id){
+        user.id = new Date().getTime()
+    }
+    saveUser(user)
+}
+
+function saveUser(user){
+    if (localStorage.hasOwnProperty(usersArray)){
+        let arrayUsers = JSON.parse(localStorage.getItem(usersArray));
+        let find = arrayUsers.find(value => value.id === user.id);
+        if (find){
+            let filter = arrayUsers.filter(value => value.id !== user.id);
+            filter.push(user);
+            localStorage.setItem(usersArray, JSON.stringify(filter));
+        }else{
+            arrayUsers.push(user)
+            localStorage.setItem(usersArray, JSON.stringify(arrayUsers))
+        }
+    }else{
+        localStorage.setItem(usersArray, JSON.stringify([user]))
+    }
+}
+
+getContactFromLS()
+function getContactFromLS() {
+    if (localStorage.hasOwnProperty(usersArray)){
+        let array = JSON.parse(localStorage.getItem(usersArray));
+        for (const user of array) {
+            contactDiv.appendChild(generateContact(user));
+        }
+    }
+}
+
+function generateContact(user) {
+    let userDiv = document.createElement('div');
+    let flag = true
+    for (const key in user) {
+        if(flag){
+            let h3 = document.createElement('h3');
+            h3.innerText = key + ':'+ user[key];
+            userDiv.appendChild(h3)
+            flag=false;
+        }else{
+            let p = document.createElement('p');
+            p.innerText = key + ':'+ user[key];
+            userDiv.appendChild(p)
+        }
+    }
+    userDiv.style = 'width: 300px'
+    let b1 = document.createElement('button');
+    let b2 = document.createElement('button');
+    b1.innerText = 'Edit'
+    b2.innerText = 'Delete'
+
+    b1.onclick =()=>{
+        editUser(user.id)
+    }
+    b2.onclick=()=>{
+        deleteUser(user.id);
+    }
+
+    userDiv.appendChild(b1);
+    userDiv.appendChild(b2);
+
+    return userDiv
+}
+
+function deleteUser(id) {
+    let user = JSON.parse(localStorage.getItem(usersArray));
+    let filter = user.filter(value => value.id !== id);
+    localStorage.setItem(usersArray, JSON.stringify(filter));
+    location.reload()
+}
+
+function editUser(id) {
+    let parsed = JSON.parse(localStorage.getItem(usersArray));
+    let user = parsed.find(value => value.id === id);
+    for (const formElement of contactForm) {
+        if (formElement.name){
+            for (const key in user) {
+                if (formElement.name === key){
+                    formElement.value = user[key]
+                }
+            }
+        }
+    }
+    tempUser=user;
+}
+
