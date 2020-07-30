7075cac03053fda1faa5e9f73846d4d1c6215f4c
Olga Ivashkiv <olgasworking@gmail.com>
2020-07-30 23:03:49 +0300

    hw10 - task1 finished

--- JS-module/hw10/hw10.js
+++ JS-module/hw10/hw10.js
@@ -1,3 +1,4 @@
+//Task 1
 function userCard(number) {
     let balance = 100;
     let transactionLimit =100;
@@ -9,30 +10,67 @@ function userCard(number) {
     }
 
     function putCredits(money){
+        let now = new Date();
+        let date = now.toLocaleDateString();
+        let time = now.toLocaleTimeString();
         balance += money;
-        return balance
+        addHistory('Received credits', money, date + ', ' + time)
+        // return balance
+    }
+
+    function addHistory(operationType, credits, operationTime) {
+        historyLogs.push({operationType, credits, operationTime})
     }
 
     function takeCredits(money) {
-        if
-        balance -= money;
-        return balance
+        let now = new Date();
+        let date = now.toLocaleDateString();
+        let time = now.toLocaleTimeString();
+        if(money<=transactionLimit && balance >= money){
+        balance -= money
+        } else {
+            console.error('Your money is over limit or you do not have enough money left')
+        }
+        addHistory('Withdrawal of credits',money,date + ', ' + time )
+        // return balance
+    }
+    
+    function setTransactionLimit(number) {
+        let now = new Date();
+        let date = now.toLocaleDateString();
+        let time = now.toLocaleTimeString();
+        transactionLimit = number;
+        addHistory('Transaction limit change',number,date + ', ' + time )
+        // return transactionLimit
     }
 
-    return {
-        getCardOptions, putCredits, takeCredits
+    function transferCredits(credit, receiverCard) {
+        let money = credit*1.005
+        if (credit<=transactionLimit && balance>=money){
+            balance = balance - money;
+            receiverCard.putCredits(credit);
+        }else{
+            console.error('Your money is over limit or you do not have enough money left')
+        }
+        // return balance
     }
+
+    return {getCardOptions, putCredits, takeCredits, setTransactionLimit, transferCredits}
 
 }
 
 const card1 = userCard(3);
-console.log(card1);
+const card2 = userCard(2);
 
 console.log(card1.getCardOptions());
-card1.putCredits(200)
-console.log(card1.getCardOptions());
+card1.putCredits(200);
 card1.takeCredits(100);
+card1.setTransactionLimit(3000);
+card1.transferCredits(10, card2);
+
 console.log(card1.getCardOptions());
+console.log('---------------');
+console.log(card2.getCardOptions());
 
 
 
