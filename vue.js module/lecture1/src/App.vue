9ecd08c28eb208180f54e6c2b0ecee513e45b1da
Olga Ivashkiv <olgasworking@gmail.com>
2020-08-20 17:36:24 +0300

    added vue.js module directory

---
+++ vue.js module/lecture1/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+  <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+
+  data(){
+    message: 'Hello World'
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue.js module/lecture1/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div >
+    <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+  data() {
+    return {
+      message: 'Hello World!!!',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
