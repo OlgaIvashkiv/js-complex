70d99f18e89805709c48a1e3f9c1ec9fba4b4edd
Olga Ivashkiv <olgasworking@gmail.com>
2020-08-27 18:06:00 +0300

    hw3 vue init commit

---
+++ vue.js module/lecture3/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input type="text" v-model="text">
+    <button @click="submitToDo">AddToDo to DB</button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      text: {}
+    }
+  },
+  methods:{
+    submitToDo(){
+      this.$http.post('https://vue-js-module-http.firebaseio.com/toDoList.json',this.text)
+              .then((res)=>{
+                console.log(res)
+              })
+    }
+  }
+}
+</script>
+
+<style>
+
+
+</style>
